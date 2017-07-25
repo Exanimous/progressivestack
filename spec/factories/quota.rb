37ab181e08cb8f0ea62347a36b628650965f4cd0
Marcus Atkinson <iwasapirateonce@gmail.com>
2017-07-25 14:46:33 +0100

    implemented stricter tests for quota_index

--- spec/factories/quota.rb
+++ spec/factories/quota.rb
@@ -11,6 +11,10 @@ FactoryGirl.define do
     f.name "testing 345 user quotum"
   end
 
+  factory :user_quotum_three, class: Quotum do |f|
+    f.name "testing 456 user quotum"
+  end
+
   factory :invalid_quotum, class: Quotum do |f|
     f.name ""
   end
--- spec/features/quota/quota_index_spec.rb
+++ spec/features/quota/quota_index_spec.rb
@@ -12,3 +12,74 @@ RSpec.feature "quota index" do
     expect(page).to have_http_status('200')
   end
 end
+
+RSpec.feature "Feature: quota index content while not signed in" do
+
+  scenario 'index displays quota_view_only', js: true do
+    Capybara.javascript_driver = :selenium
+    user_one = FactoryGirl.create(:user)
+    user_two = FactoryGirl.create(:user_two)
+
+    quotum_one = FactoryGirl.create(:user_quotum, tenant_id: user_one.tenant_ids.first)
+    quotum_two = FactoryGirl.create(:user_quotum_two, tenant_id: user_two.tenant_ids.first)
+    quotum_nil = FactoryGirl.create(:user_quotum_three, tenant_id: nil)
+
+    visit quota_path
+
+    expect(page).to have_content("#{quotum_one.name} (view only)")
+    expect(page).to have_content("#{quotum_two.name} (view only)")
+    expect(page).to have_content("#{quotum_nil.name} (view only)")
+  end
+end
+
+RSpec.feature "Feature: quota index content while signed in as user" do
+  before :each do
+    @user = FactoryGirl.create(:user)
+    login_as @user
+  end
+
+  scenario 'index displays quota_controllable and quota_view_only', js: true do
+    Capybara.javascript_driver = :selenium
+    user_two = FactoryGirl.create(:user_two)
+
+    quotum = FactoryGirl.create(:user_quotum, tenant_id: @user.tenant_ids.first)
+    quotum_two = FactoryGirl.create(:user_quotum_two, tenant_id: user_two.tenant_ids.first)
+    quotum_nil = FactoryGirl.create(:user_quotum_three, tenant_id: nil)
+
+    visit quota_path
+
+    expect(page).to have_content(quotum.name)
+    expect(page).to have_content("#{quotum_two.name} (view only)")
+    expect(page).to have_content("#{quotum_nil.name} (view only)")
+  end
+end
+
+RSpec.feature "Feature: quota index content while signed in as guest" do
+  # simulate guest creation
+  before :each do
+    @quotum_guest = FactoryGirl.build(:user_quotum)
+    visit quota_path
+
+    click_link "New Quotum (remote)"
+
+    wait_for_ajax
+    fill_in 'Name', with: @quotum_guest[:name]
+
+    click_button "Create Quotum"
+    wait_for_ajax
+  end
+
+  scenario 'index displays quota_controllable and quota_view_only', js: true do
+    Capybara.javascript_driver = :selenium
+    user_two = FactoryGirl.create(:user_two)
+
+    quotum_two = FactoryGirl.create(:user_quotum_two, tenant_id: user_two.tenant_ids.first)
+    quotum_nil = FactoryGirl.create(:user_quotum_three, tenant_id: nil)
+
+    visit quota_path
+
+    expect(page).to have_content(@quotum_guest.name)
+    expect(page).to have_content("#{quotum_two.name} (view only)")
+    expect(page).to have_content("#{quotum_nil.name} (view only)")
+  end
+end
